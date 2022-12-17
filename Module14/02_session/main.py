b05e39ddbd62caa56f5ac89285e6c940b3cea8ab
Vadim <vanderlion@rambler.ru>
2022-12-17 20:56:51 +0400

    added m14 homework, 14.2 fix: session

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+print("Задача 2. Сессия")
+
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+print("Уравнение прямой, проходящей через эти точки:")
+x_diff = x1 - x2
+y_diff = y1 - y2
+if x_diff == 0:
+    print("x = ", x1)
+elif y_diff == 0:
+    print("y = ", y1)
+else:
+    k = y_diff / x_diff
+    b = y2 - k * x2
+    print("y = ", k, " * x + ", b)
+
--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+print("Задача 3. Сумма и разность")
+
+def count_of_digits(n):
+    return len(str(n))
+
+def sum_of_digits(n):
+    return sum(int(i) for i in str(n))
+
+num = int(input("Введите число: "))
+
+sums = sum_of_digits(num)
+counts = count_of_digits(num)
+print("\nСумма цифр:", sums)
+print("\nКол-во цифр в числе:", counts)
+
+print("\nРазность суммы и кол-ва цифр:", sums - counts)
