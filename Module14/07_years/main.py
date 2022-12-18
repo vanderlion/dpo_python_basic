26b709adc16b4a13cbf983626df7003f48157cc6
@vadim_bubnov_1 <vanderlion@rambler.ru>
2022-12-18 18:52:06 +0400

    added m14 homework, 14.7 fix: years Честно скажу подсмотрел)

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+p=[]
+for i in range(int(input('Перый год? ')),int(input('Второй год? '))+1):
+    l=list(str(i))
+    s=set(l)
+    if len(s)==2:
+        for j in s:
+            if l.count(j)!=2: p.append(i); break
+print (*p, sep='\n')
