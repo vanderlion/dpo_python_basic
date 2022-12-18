af86c03c07ad3a2b0a3990da012378fc0feedf91
@vadim_bubnov_1 <vanderlion@rambler.ru>
2022-12-18 18:01:09 +0400

    added m14 homework, 14.5 fix: least divisor

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,14 @@
-# TODO здесь писать код
+print("Задача 4. Число наоборот 3")
 
+def reverse_float(num):
+    parts = str(num).split(".")
+    reversed_parts = ["".join(reversed(part)) for part in parts]
 
+    return float( ".".join(reversed_parts) )
+
+a = reverse_float(input("Введите первое число: "))
+b = reverse_float(input("Введите второе число: "))
+
+print("Первое число наоборот: ", a)
+print("Второе число наоборот: ", b)
+print("Сумма обратных чисел", a + b)
