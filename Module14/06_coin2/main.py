8c249b7d16a2676169691b01b0e1cb695f5873f0
@vadim_bubnov_1 <vanderlion@rambler.ru>
2022-12-18 18:28:24 +0400

    added m14 homework, 14.6 fix: coin2

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def find_monetka(c1,c2,r):
+    if c1 <= r and c2 <= r:
+        print("Монетка где-то рядом")
+    else:
+        print("Монетки в области нет")
+x_m=float(input("Введите координату x:"))
+y_m=float(input("Введите координату y:"))
+r=int(input("Введите радиус r:"))
+find_monetka(x_m, y_m, r)
