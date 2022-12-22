5241cacbc554c5a7b6b2efaed491c6cb5a4526f3
@vadim_bubnov_1 <vanderlion@rambler.ru>
2022-12-22 20:04:13 +0400

    added m15 homework, 15.2 fix: tournament

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def display_result(participants_names):
+
+    print("Первый день:", participants_names)
+
+def get_participants_names(names):
+
+    names_list = ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
+    names = []
+    for i in range(0, len(names_list) - 1, 2):
+        names.append(names_list[i])
+
+    return names
+
+if __name__ == "__main__":
+    participants_names = get_participants_names(
+        ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
+    )
+    display_result(participants_names)
