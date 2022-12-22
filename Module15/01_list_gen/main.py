413113224477da264912fdc42e86c70332084b50
@vadim_bubnov_1 <vanderlion@rambler.ru>
2022-12-22 19:59:45 +0400

    added m15 homework, 15.1 fix: list_gen

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+number_list = []
+number = int(input("Введите целое число: "))
+
+for i in range(1, number):
+    if i % 2 != 0:
+        number_list.append(i)
+
+print("Список из нечётных чисел от одного до N", number_list)
