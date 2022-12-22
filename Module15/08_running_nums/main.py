6a0f294c7ede3816ef25da9bbc77ae10f170031c
@vadim_bubnov_1 <vanderlion@rambler.ru>
2022-12-22 21:15:32 +0400

    added m15 homework, 15.8 fix: running nums

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+N = int(input("Кол-во элементов: "))
+start_list = []
+finish_list = []
+
+for _ in range(N):
+    element = input("Введите элемент списка:")
+    start_list.append(element)
+
+K = int(input("Сдвиг: "))
+index = 0
+while index < N - 1:
+    empty_place = start_list[index]
+    start_list[index] = start_list[index - K]
+    start_list[index - K] = empty_place
+    index += 1
+
+print(start_list)
