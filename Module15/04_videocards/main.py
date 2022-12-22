e8e085c28a2c4ff61b9bc8f8576f55567f2620e7
@vadim_bubnov_1 <vanderlion@rambler.ru>
2022-12-22 20:16:05 +0400

    added m15 homework, 15.4 fix: videocards

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,20 @@
-# TODO здесь писать код
+videocard_list = []
+new_list = []
+newest_list = []
+num_video = int(input("Кол-во видеокарт: "))
+count = 1
+
+for _ in range(num_video):
+    card = int(input(str(count) + " Видеокарта: "))
+    count += 1
+    videocard_list.append(card)
+
+print("Старый список видеокарт: ", *videocard_list)
+
+newest_list = max(videocard_list)
+new_list = [i for i in videocard_list if i != newest_list]
+print("Новый список видеокарт: ", new_list)
+
+
 
 
