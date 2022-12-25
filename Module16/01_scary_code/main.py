56ec508f0e53d3608bc6fb0e0d47a5f53d999d20
Vadim <vanderlion@rambler.ru>
2022-12-26 00:54:57 +0400

    added m16 homework, 16.1 fix: scary code

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,14 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+one = [1, 5, 3]
+two = [1, 5, 1, 5]
+three = [1, 3, 1, 5, 3, 3]
 
-# TODO переписать программу
+one.extend(two)
+number_of_digits_five = one.count(5)
+print("Кол-во цифр 5 при первом объединении: ",number_of_digits_five)
+for _ in range(number_of_digits_five):
+    one.remove(5)
+
+one.extend(three)
+number_of_digits_three = one.count(3)
+print("Кол-во цифр 3 при втором объединении: ", number_of_digits_three)
+print(one)
