f3525d2b0c4d07b2afca45280d37849a6c1c1a52
Vadim <vanderlion@rambler.ru>
2022-12-26 00:57:52 +0400

    added m16 homework, 16.5 fix: songs

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -10,4 +10,25 @@ violator_songs = [
     ['Clean', 5.83]
 ]
 
-# TODO здесь писать код
+
+li = []
+special_list = []
+qty = int(input('Сколько песен выбрать? '))
+print()
+count = 1
+
+for _ in range(qty):
+    song = input('Введите название ' + str(count) + ' песни: ')
+    for elem in violator_songs:
+        li.extend(elem)
+        if song not in li:
+            print('Ошибка. Такой песни в плейлисте нет!')
+            break
+        else:
+            special_list.append(song)
+            count += 1
+            summ = 0
+for i_time in violator_songs:
+    if i_time[0] in special_list:
+        summ += i_time[1]
+print('\nОбщее время звучания песен:', float(round(summ, 2)))
