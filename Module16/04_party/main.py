42f5f34ac481bf635458977f3a8bf86e5dfeb758
Vadim <vanderlion@rambler.ru>
2022-12-26 00:56:19 +0400

    added m16 homework, 16.4 fix: party

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,21 @@
+guest_counts=5
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+while True:
+    guest=input('Гость пришел или ушел?: ')
+    name=input('Введите имя гостя: ')
 
-# TODO здесь писать код
+    if guest=='пришел':
+        print('Привет',name)
+        guests.append(name)
+        print('Сейчас на вечеринке',guest_counts+1, 'человек',guests)
+        guest_counts+1
+
+    if guest=='ушел':
+        print('Пока',name)
+        print('Сейчас на вечеринке',guest_counts-1, 'человек ',guests)
+        guests.remove(1)
+        guest_counts-=1
+
+    if guest=='пора спать':
+        print('Вечеринка закончилась, все легли спать')
+    break
