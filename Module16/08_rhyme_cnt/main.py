8f0428e688b842e58b0aaf22655c2224db1a9a4e
Vadim <vanderlion@rambler.ru>
2022-12-27 22:48:38 +0400

    added m16 homework, 16.8 fix: rhyme_cnt

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+amount_peaoples = int(input('Кол-во человек: '))
+number = int(input('Какое число в считалке?: '))
+print('Значит, выбывает каждый', number, 'человек')
+
+list_peoples = list(range(1, amount_peaoples + 1))
+count = 0
+
+while len(list_peoples) > 1:
+    print('Текущий круг людей: ', list_peoples)
+    print('Начало счёта с номера:', list_peoples[count])
+    count = (count + number - 1) % len(list_peoples)
+    if list_peoples[count] == list_peoples[-1]:
+        print('Выбывает человек под номером:', list_peoples.pop(count))
+        count = 0
+    else:
+        print('Выбывает человек под номером:', list_peoples.pop(count))
+print('Остался человек под номером:', list_peoples[0])
