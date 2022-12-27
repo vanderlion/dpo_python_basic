bab0aee18941e19cb99b755dd8ffa4802cb28e5e
Vadim <vanderlion@rambler.ru>
2022-12-27 22:11:26 +0400

    added m16 homework, 16.7 fix: roler skates

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+shoes_size = []
+feet_size = []
+suitable_list = []
+
+N = int(input("Кол-во коньков: "))
+for number in range(N):
+    print("Размер ", number + 1, " пары: ", end="")
+    size = int(input())
+    shoes_size.append(size)
+
+K = int(input("Кол-во людей: "))
+for number in range(K):
+    print("Размер ноги ", number + 1, " человека: ", end="")
+    size = int(input())
+    feet_size.append(size)
+
+for index_shoes in range(N):
+    for index_feet in range(K):
+        if shoes_size[index_shoes] == feet_size[index_feet]:
+            suitable_list.append(feet_size[index_feet])
+
+unique_list = list(set(suitable_list))
+print("Наибольшее кол-во людей, которые могут взять ролики: ", len(unique_list))
+
