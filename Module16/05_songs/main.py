060b7ea8c7703c11be6b3570970150d43465eae7
Vadim <vanderlion@rambler.ru>
2022-12-27 22:00:12 +0400

    added m16 homework, 16.5 fix: songs

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -1,34 +1,22 @@
 violator_songs = [
-    ['World in My Eyes', 4.86],
-    ['Sweetest Perfection', 4.43],
-    ['Personal Jesus', 4.56],
-    ['Halo', 4.9],
-    ['Waiting for the Night', 6.07],
-    ['Enjoy the Silence', 4.20],
-    ['Policy of Truth', 4.76],
-    ['Blue Dress', 4.29],
-    ['Clean', 5.83]
+    ["World in My Eyes", 4.86],
+    ["Sweetest Perfection", 4.43],
+    ["Personal Jesus", 4.56],
+    ["Halo", 4.9],
+    ["Waiting for the Night", 6.07],
+    ["Enjoy the Silence", 4.20],
+    ["Policy of Truth", 4.76],
+    ["Blue Dress", 4.29],
+    ["Clean", 5.83]
 ]
 
-
-li = []
-special_list = []
-qty = int(input('Сколько песен выбрать? '))
-print()
-count = 1
-
-for _ in range(qty):
-    song = input('Введите название ' + str(count) + ' песни: ')
-    for elem in violator_songs:
-        li.extend(elem)
-        if song not in li:
-            print('Ошибка. Такой песни в плейлисте нет!')
-            break
-        else:
-            special_list.append(song)
-            count += 1
-            summ = 0
-for i_time in violator_songs:
-    if i_time[0] in special_list:
-        summ += i_time[1]
-print('\nОбщее время звучания песен:', float(round(summ, 2)))
+custom_songs = []
+songs = int(input("Сколько песен выбрать? "))
+summ = 0
+for i in range(songs):
+    song = input('Введите название ' + str(i + 1) + ' песни: ')
+    custom_songs.append(song)
+for k in violator_songs:
+    if k[0] in custom_songs:
+        summ += k[1]
+print("\nОбщее время звучания песен:", float(round(summ, 2)))
