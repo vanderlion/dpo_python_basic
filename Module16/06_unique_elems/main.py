d83b5ebce504989197dc069f2744f7a14d877787
Vadim <vanderlion@rambler.ru>
2022-12-27 22:04:54 +0400

    added m16 homework, 16.6 fix: unique elems

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+first_list = []
+second_list = []
+
+for i in range(1, 4):
+    print('Введите', i, 'число для первого списка:', end=' ')
+    number = int(input())
+    first_list.append(number)
+
+for i in range(1, 8):
+    print('Введите', i, 'число для второго списка:', end=' ')
+    number = int(input())
+    second_list.append(number)
+
+first_list.extend(second_list)
+for _ in range(len(first_list)):
+    for i in first_list:
+        if first_list.count(i) > 1:
+            first_list.remove(i)
+print(first_list)
+print(second_list)
