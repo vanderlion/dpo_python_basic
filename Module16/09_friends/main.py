263ecfd20ee9c942162ab39c43bf6b0ae85d27f6
Vadim <vanderlion@rambler.ru>
2022-12-27 23:03:08 +0400

    added m16 homework, 16.9 fix: friends

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+N = int(input("Кол-во друзей: "))
+K = int(input("Кол-во долговых расписок: "))
+friends_list = []
+
+for _ in range(N):
+    friends_list.append(0)
+
+for number in range(K):
+    print("\n", number + 1, "расписка: ")
+    from_who = int(input("От кого: "))
+    to_who = int(input("Кому: "))
+    how_much = int(input("Сколько: "))
+    friends_list[from_who - 1] -= how_much
+    friends_list[to_who - 1] += how_much
+
+print("Баланс друзей: ")
+for index in range(len(friends_list)):
+    print(index + 1, ": ", friends_list[index])
