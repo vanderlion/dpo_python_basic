41a7cff845ea042a21a50414973ef0539970f767
Vadim <vanderlion@rambler.ru>
2023-01-07 18:16:37 +0400

    added m17 homework, 17.6 fix: list compression

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+import random
+
+n = input("Кол-во чисел в списке: ")
+list1 = [random.randint(0, 2) for _ in range(int(n))]
+print("Список до сжатия:", list1)
+list2 = [x for x in list1 if x]
+print("Список после сжатия: ", list2)
