fc546cc0c1fc20b1c5f31eab6f451b56e057a443
Vadim <vanderlion@rambler.ru>
2023-01-07 19:54:11 +0400

    added m17 homework, 17.9 fix: list of lists 2

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,5 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+flat = [num for row in nice_list for row_i in row for num in row_i]
+print(flat)
