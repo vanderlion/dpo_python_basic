2c4efffaf4d3f66ef735b8be7576d796c67a609d
Vadim <vanderlion@rambler.ru>
2023-01-11 10:38:47 +0400

    added m15 homework, 18.2 fix: longest word

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,3 @@
-# TODO здесь писать код
+ls = input('Введите текст: ').split(' ')
+max_len = max([len(x) for x in ls])
+print("Самое длинное слово", [x for x in ls if len(x) == max_len], "\nДлина этого слова:", max_len)
