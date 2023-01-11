bb4460aa14d5fb03227d767e16f5d8c7ebd02c40
Vadim <vanderlion@rambler.ru>
2023-01-11 10:53:14 +0400

    added m15 homework, 18.3 fix: files

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+document = input("Название файла: ")
+
+if document.startswith(("@", "№", "$", "%", "^", "&", "*", "(", ")")):
+    print("Ошибка: название начинается на один из специальных символов")
+elif not document.endswith('.txt') or document.endswith('.docx'):
+    print("Ошибка: неверное расширение файла. Ожидалось .txt или .docx")
+else:
+    print("Файл назван верно", document)
