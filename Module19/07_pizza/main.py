23d8007bdb096ef508855f3b1adf0ba33cbe8436
Vadim <vanderlion@rambler.ru>
2023-01-14 15:52:35 +0400

    added M19 homework, 19.7 fix: pizza

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def fun(dct, key, arg):
+    dct[key] = dct.setdefault(key, 0) + int(arg)
+    return dct
+
+
+d = {}
+for _ in range(int(input('Введите кол-во заказов: '))):
+    _, fio, pizza, amount = input().rsplit(maxsplit=3)
+    d[fio] = fun(d.get(fio, {}), pizza, amount)
+
+for name in sorted(d):
+    print(f'{name}:')
+    for pizza, amount in sorted(d.get(name).items()):
+        print(f'{pizza}: {amount}')
+    print()
