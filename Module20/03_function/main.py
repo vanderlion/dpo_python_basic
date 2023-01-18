bb893a3c9b57a02b68d47417fd934fa855f232c1
Vadim <vanderlion@rambler.ru>
2023-01-18 16:41:51 +0400

    added m20 homework, 20.3 fix: function

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def slicer(x):
+    for num in x:
+        try:
+            b = int(num)
+            if b != num: return x
+        except: return x
+    num = list(x)
+    num.sort()
+    return tuple(num)
