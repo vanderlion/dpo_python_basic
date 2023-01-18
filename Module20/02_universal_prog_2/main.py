e5d374adb74aa8107e79b7519d8545ad662447f1
Vadim <vanderlion@rambler.ru>
2023-01-18 16:26:03 +0400

    added m20 homework, 20.2 fix: universal_prog_2

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def crypto(checking_list):
+    return [v for i, v in enumerate(checking_list) if i >= 2 and is_prime(i)]
+
+def is_prime(i_num):
+    k = 0
+    for i in range(2, i_num // 2 + 1):
+        if i_num % i == 0:
+            k = k + 1
+    if k <= 0:
+        return True
+    else:
+        return False
+
+print(crypto([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(crypto('О Дивный Новый мир!'))
