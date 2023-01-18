c2f6a6a02af856c40be5bbdd3182b804bbc78a9b
Vadim <vanderlion@rambler.ru>
2023-01-18 17:04:37 +0400

    added m20 homework, 20.5 fix: one family

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,38 @@
-# TODO здесь писать код
+all_families_dict = {
+
+1: {
+
+"Питонов Никита": 35,
+
+"Питонова Алина": 34,
+
+"Питонов Павел": 10
+
+},
+
+2: {
+
+"Програмистов Акакий": 115,
+
+"Програмистова Зульфия": 112,
+
+"Програмистов Корнеплод": 9,
+
+"Програмистова Кочебрыжка": 10
+
+}
+
+}
+
+
+
+family = input("Введите фамилию: ")
+
+if list(family)[-1] == 'a':
+    family = family[::-1]
+
+print(family)
+for key in all_families_dict.keys():
+    for values in all_families_dict[key].keys():
+        if family[:-1] in values:
+            print(values, " ", all_families_dict[key][values])
