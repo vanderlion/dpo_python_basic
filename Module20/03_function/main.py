84c6a1ed542973de6b19a1f4eca055ce9b022155
Vadim <vanderlion@rambler.ru>
2023-01-19 16:45:06 +0400

    added m20 homework, 20.3 fix: function

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,9 +1,8 @@
-def slicer(x):
-    for num in x:
-        try:
-            b = int(num)
-            if b != num: return x
-        except: return x
-    num = list(x)
-    num.sort()
-    return tuple(num)
+def slicer(t, num):
+    if num not in t:
+        return ()
+    if t.count(num) == 1:
+        return t[t.index(num):]
+    return t[t.index(num):t.index(num, t.index(num) + 1) + 1]
+
+print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2))
