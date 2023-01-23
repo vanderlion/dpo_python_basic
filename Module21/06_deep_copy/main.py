ec9de88136ba488c4eaffcc08c1862de43f3db1f
@vadim_bubnov_1 <vanderlion@rambler.ru>
2023-01-23 19:58:19 +0400

    added M21 homework,21.9 fix: Deep copy

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -10,5 +10,25 @@ site = {
         }
     }
 }
+def find_key(struct, key, meaning):
+    if key in struct:
+        struct[key] = meaning
+        return site
 
-# TODO здесь писать код
+    for sub_struct in struct.values():
+        if isinstance(sub_struct, dict):
+            result = find_key(sub_struct, key, meaning)
+            if result:
+                return site
+
+
+
+number_sites = int(input('Сколько сайтов: '))
+for _ in range(number_sites):
+    product_name = input('Введите название продукта для нового сайта: ')
+    key = {'title': f'Куплю/продам {product_name} недорого', 'h2': f'У нас самая низкая цена на {product_name}'}
+    for i in key:
+        find_key(site, i, key[i])
+
+    print(f'Сайт для {product_name}:')
+    print(site, '\n')
