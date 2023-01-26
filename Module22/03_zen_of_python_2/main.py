89455bbcad227e4a77e257071ce35b069f971f21
@vadim_bubnov_1 <vanderlion@rambler.ru>
2023-01-26 23:31:03 +0400

    added m15 homework, 22.3 fix: Zen of Python 2

--- Module22/03_zen_of_python_2/main.py
+++ Module22/03_zen_of_python_2/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+text = open("zen.txt").read().lower()
+letters = [c for c in text if c in "abcdefghijklmnopqrstuvwxyz"]
+print("Количество букв в файле: ", len(letters),
+      "Количество слов в файле: ", len(text.split()),
+      "Количество строк в файле :", len(text.split("\n")),
+      "Наиболее редкая буква: ", min(letters, key=letters.count), sep="\n")
