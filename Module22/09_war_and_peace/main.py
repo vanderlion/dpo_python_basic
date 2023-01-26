cabd7c784fb00e56d34b4542b84853fbf1d9c703
@vadim_bubnov_1 <vanderlion@rambler.ru>
2023-01-27 00:14:19 +0400

    added M22 homework, 22.9 fix: War and peace

--- Module22/09_war_and_peace/main.py
+++ Module22/09_war_and_peace/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import zipfile
+
+dictionary = {}
+with zipfile.ZipFile("voyna-i-mir.zip") as archive:
+    for filename in archive.filelist:
+        with archive.open(filename) as file:
+            for letter in file.read().decode():
+                if letter not in dictionary:
+                    dictionary[letter] = 0
+                dictionary[letter] += 1
+
+for letter, amount in dictionary.items():
+    print(letter, amount)
