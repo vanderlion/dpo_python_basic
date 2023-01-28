a7e47a43010dc5d95e480973943c44f67099874e
Vadim <vanderlion@rambler.ru>
2023-01-28 11:26:00 +0400

    added m23 homework, 23.5 fix: text calc

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+result = [ ]
+with open('calc.txt', 'r') as file:
+    for line in file.readlines():
+        try:
+            result.append(eval(line))
+        except:
+            if input('Обнаружена ошибка:' + line + 'Хотите исправить? ') == 'да':
+                line = input('Введите исправленную строку: ')
+                result.append(eval(line))
+print("Сумма результатов:", sum(result))
