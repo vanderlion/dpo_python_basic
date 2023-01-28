06353ee90b5b52f7083bf9e75318205054ea4a99
Vadim <vanderlion@rambler.ru>
2023-01-28 09:58:00 +0400

    added m23 homework, 23.2 fix: names 2

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -1,39 +1,40 @@
 import random
 
+result = list()
 
-def f(x, y):
+
+def func1(x, y):
     x += random.randint(0, 10)
     y += random.randint(0, 5)
     return x / y
 
 
-def f2(x, y):
+def func2(x, y):
     x -= random.randint(0, 10)
     y -= random.randint(0, 5)
     return y / x
 
 
-try:
-    file = open('coordinates.txt', 'r')
-    for line in file:
-        nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
-        try:
-            res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
-except Exception:
-    print("Что-то пошло не так с первой функцией")
+with open('coordinates.txt') as f:
+    data = f.readlines()
 
+for line in data:
+    line = line.split()
 
-# TODO отредактировать и исправить программу
+    try:
+        num1, num2 = map(int, line)
+    except:
+        print('Некорректный данные в файле "coordinates.txt"')
+        break
+
+    res1 = func1(num1, num2)
+    res2 = func2(num1, num2)
+    number = random.randint(0, 100)
+
+    my_list = sorted([res1, res2, number])
+    my_list = list(map(str, my_list))
+
+    result.append(' '.join(my_list) + '\n')
+
+with open('result.txt', 'w') as f:
+    f.writelines(result)
