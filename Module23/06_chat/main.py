fd29fadb6689d35ab11b044620186b441578b2ef
Vadim <vanderlion@rambler.ru>
2023-01-28 11:29:07 +0400

    added m23 homework, 23.6 fix: chat

--- Module23/06_chat/main.py
+++ Module23/06_chat/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+user = input('Введите имя: ')
+
+while True:
+    print('Чтобы посмотреть текущий текст чата - введите <0>')
+    print('Чтобы отправить сообщение - введите <1>')
+    action = input()
+
+    if action == '0':
+        try:
+            with open('chat.txt', 'r', encoding='utf-8') as file:
+                for i_message in file:
+                    print(i_message, end='')
+        except FileNotFoundError:
+            print('История сообщений пуста. \n')
+
+    elif action == '1':
+        message = input('Введите сообщение: ')
+        with open('chat.txt', 'a', encoding='utf-8') as file:
+        file.write(f'{user}:{message}\n')
+    else:
+        print('Такой команды нет')
