20c4ac3a9be864bb21cd27749df0a2bc8f03809d
Vadim <vanderlion@rambler.ru>
2023-01-30 13:05:43 +0400

    added m24 homework, 24.6 fix: Magic

--- Module24/06_magic/main.py
+++ Module24/06_magic/main.py
@@ -1,1 +1,77 @@
-# TODO здесь писать код
+class Fire:
+    title = 'Огонь'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Lightning()
+        elif isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Lava()
+
+
+class Air:
+    title = 'Воздух'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Shtorm()
+        elif isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Earth):
+            return Dust()
+
+
+class Water:
+    title = 'Вода'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Shtorm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Dirt()
+
+
+class Earth:
+    title = 'Земля'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt()
+        elif isinstance(other, Fire):
+            return Lava()
+        elif isinstance(other, Air):
+            return Dust()
+
+
+class Shtorm:
+    title = 'Шторм'
+
+
+class Steam:
+    title = 'Пар'
+
+
+class Dirt:
+    title = 'Грязь'
+
+
+class Lightning:
+    title = 'Молния'
+
+
+class Dust:
+    title = 'Пыль'
+
+
+class Lava:
+    title = 'Лава'
+
+
+f = Water()
+a = Air()
+s = f + a
+
+print(f"Смешиваем '{f.title}' c '{a.title}' и получаем '{s.title}'")
