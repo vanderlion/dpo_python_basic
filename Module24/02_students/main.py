dfb420911e1ea833b3434af57eaa20b9e336e296
Vadim <vanderlion@rambler.ru>
2023-01-30 12:24:18 +0400

    added m24 homework, 24.2 fix: Students

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,1 +1,42 @@
-# TODO здесь писать код
+class Student:
+
+    def __init__(self, full_name, group_number, progress):
+        self.full_name = full_name
+        self.group_number = group_number
+        self.progress = progress
+        self.average = sum(progress) / len(progress)
+
+    def give_average(self):
+        return self.average
+
+    def __str__(self):
+        return f'{self.full_name} {self.group_number}'
+
+    def good(self):
+        good_progress = list(filter(lambda x: x in [4, 5], self.progress))
+        return self.progress == good_progress
+
+
+def receiving_data():
+    name = input('ФИО студента: ')
+    group = input('номер группы: ')
+    ball = list(map(int, input('оценки через пробел: ').split()))
+    return name, group, ball
+
+
+list_student = [Student(*receiving_data()) for _ in range(10)]
+print('список студентов')
+for student in list_student:
+    print(student)
+print()
+
+list_student.sort(key=lambda x: x.give_average())
+print('список студентов отсортированный')
+for student in list_student:
+    print(student)
+print()
+
+print('студенты с оценками 4 и 5')
+for student in list_student:
+    if student.good():
+        print(student)
