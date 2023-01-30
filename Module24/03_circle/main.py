4d1f910ea478c68bbce546c234038bb2e0d61e7e
Vadim <vanderlion@rambler.ru>
2023-01-30 12:34:55 +0400

    added m24 homework, 24.3 fix: Circle

--- Module24/03_circle/main.py
+++ Module24/03_circle/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+class Circle:
+    pi = 3.14159
+
+    def __init__(self, x=0, y=0, r=1):
+        self.x = x
+        self.y = y
+        self.r = r
+
+    def get_area(self):
+        return self.r * self.r * self.pi
+
+    def get_perimeter(self):
+        return 2 * self.r * self.pi
+
+    def scale(self, k):
+        self.r *= k
+
+    def is_intersect(self, other):
+        return (self.x - other.x) ** 2 + (self.y - other.y) ** 2 <= (self.r + other.r) ** 2
