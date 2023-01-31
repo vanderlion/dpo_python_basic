f022b1c82ddfe4aa87c48b1ec9edadbdd0306b8b
@vadim_bubnov_1 <vanderlion@rambler.ru>
2023-01-31 21:30:04 +0400

    added M25 homework, 25.4 fix: company

--- Module25/04_company/main.py
+++ Module25/04_company/main.py
@@ -1,1 +1,73 @@
-# TODO здесь писать код
+from random import randint, choice
+
+NAMES = ['Александр', 'Форофонт', 'Евдоким', 'Силантий', 'Игнат', 'Елисей', 'Пафнутий']
+SURNAMES = ['Иванов', 'Петров', 'Сидоров', 'Пупкин', 'Попков']
+
+
+# generate randon name, surname, age from constants arrays above
+def generate_person():
+    name = choice(NAMES)
+    surname = choice(SURNAMES)
+    age = randint(20, 50)
+    return name, surname, age
+
+
+class Person:
+    def __init__(self, name, surname, age):
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+
+    def __str__(self):
+        return f'Меня зовут {self.__name} {self.__surname}. Мой возраст - {self.__age}'
+
+
+class Employee(Person):
+    def calc_salary(self):
+        raise Exception('This method must be overriden')
+
+    def __str__(self):
+        return super().__str__() + f'\nМоя зарплата {self.calc_salary()}'
+
+
+class Manager(Employee):
+    def calc_salary(self):
+        return 13000
+
+
+class Agent(Employee):
+    sales: int
+
+    def calc_salary(self):
+        return 5000 + .05 * self.sales
+
+
+class Worker(Employee):
+    hours: int
+
+    def calc_salary(self):
+        return 100 * self.hours
+
+
+if __name__ == '__main__':
+    employees = list()
+
+    # managers
+    for _ in range(3):
+        employees.append(Manager(*generate_person()))
+
+    # agents
+    for _ in range(3):
+        agent = Agent(*generate_person())
+        agent.sales = randint(2000, 10000)
+        employees.append(agent)
+
+    # workers
+    for _ in range(3):
+        worker = Worker(*generate_person())
+        worker.hours = randint(20, 50)
+        employees.append(worker)
+
+    # logging out
+    for emp in employees:
+        print(emp)
