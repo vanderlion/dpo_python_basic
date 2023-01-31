de3b3208e58d8a64688d39a0f23c0ecc6f66e012
@vadim_bubnov_1 <vanderlion@rambler.ru>
2023-01-31 21:09:07 +0400

    added M25 homework, 25.3 fix: my dict

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def get(self, key, default_value=0):
+        if key in self:
+            return self[key]
+        else:
+            return default_value
+
+
+new_dict = MyDict()
+new_dict['Ikari'] = 1
+new_dict['Asuka'] = 2
+new_dict['Rei'] = 3
+print(new_dict)
+print(new_dict.get('Misato'))
