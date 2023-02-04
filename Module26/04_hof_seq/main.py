202b0da163161036e2d7732d7699a26df8527727
Vadim <vanderlion@rambler.ru>
2023-02-04 20:35:30 +0400

    added M26 homework, 26.4 fix: hof seq

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+def QHofstadter(s):
+    if s == [1, 1]:
+        a = s[:]
+        while True:
+            try:
+                q = a[-a[-1]] + a[-a[-2]]
+                a.append(q)
+                yield q
+            except IndexError:
+                return
+    else:
+        print('Передано неверное значение списка!\nПравильный список [1, 1]')
+
+
+Q = [1, 1]
+
+for i, number in enumerate(QHofstadter(Q)):
+    if i <= 30:
+        print(number, end=', ')
+    else:
+        break
