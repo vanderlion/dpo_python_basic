9b69d8746d618be67532fbde50fc41fec62df9b2
Vadim <vanderlion@rambler.ru>
2023-02-04 21:32:29 +0400

    added M26 homework, 26.5 fix: str count

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+import os
+from collections.abc import Iterable
+
+
+def strings_count(directory: str) -> Iterable[tuple]:
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            count = 0
+            if os.path.join(root, file).endswith('.py'):
+                curr_file = open(os.path.join(root, file), 'r', encoding='utf-8')
+                for line in curr_file.readlines():
+                    if not (line == '\n' or line.strip().startswith(('"', '#', "'"))):
+                        count += 1
+                yield os.path.join(root, file), count
+
+
+for element in strings_count(directory='..'):
+    print('Файл "{}": строк кода - {}'.format(element[0], element[1]))
