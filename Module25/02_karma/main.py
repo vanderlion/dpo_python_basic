2188b2274c918b433647bbd9f0609cd66d3b8ce7
Vadim <vanderlion@rambler.ru>
2023-02-04 11:26:21 +0400

    added M25 homework, 25.2 fix: karma

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -21,20 +21,21 @@ class DepressionError(Exception):
         return 'Строка с ошибкой'
 
 
-def one_day(count):
+def one_day():
     day_karma = random.randint(1, 7)
-    if random.randint(1, 10) == 1:
-        with open('karma.log', 'w') as karma_log:
-            exception = random.choice([KillError(), DrunkError(), CarCrashError(), GluttonyError(), DepressionError()])
-            karma_log.write(f'день {count}: {exception}\n')
-            raise exception
+
+    if random.randint(1, 10) == 5:
+        exception = random.choice([KillError(), DrunkError(), CarCrashError(), GluttonyError(), DepressionError()])
+        raise exception
+
     return day_karma
 
 karma = 0
-day = 0
-while karma < 500:
-    day += 1
-    if one_day(day):
-        pass
-    else:
-        print("Просветление достигнуто")
+with open('karma.log', 'w', encoding='utf-8') as file:
+    while karma < 500:
+        try:
+            karma += one_day()
+        except (KillError, DrunkError, CarCrashError, GluttonyError, DepressionError) as error:
+            file.write(f'Поймано исключение {error}\n')
+        else:
+            print("Просветление достигнуто")
