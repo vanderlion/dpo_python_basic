4e1d1963df650e3b8ba51e2a10749ea84786fef6
Vadim <vanderlion@rambler.ru>
2023-02-04 20:24:47 +0400

    added M26 homework, 26.2 fix: refactoring

--- Module26/02_refactoring/main.py
+++ Module26/02_refactoring/main.py
@@ -2,16 +2,16 @@ list_1 = [2, 5, 7, 10]
 list_2 = [3, 8, 4, 9]
 to_find = 56
 
-can_continue = True
-for x in list_1:
-    for y in list_2:
-        result = x * y
-        print(x, y, result)
-        if result == to_find:
-            print('Found!!!')
-            can_continue = False
-            break
-    if not can_continue:
-        break
 
-# TODO провести рефакторинг кода
+def func(to_find):
+    for x in list_1:
+        for y in list_2:
+            result = x * y
+            yield x, y, result
+            if result == to_find:
+                print('Found!!!')
+    return
+
+
+for num in func(to_find):
+    print(str(num)[1:-1])
