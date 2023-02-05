6bcbaf1205cf54855c9511d2ec7317d1467e8a92
Vadim <vanderlion@rambler.ru>
2023-02-05 22:36:58 +0400

    added m27 homework, 27.4 fix: debug

--- Module27/04_debug/main.py
+++ Module27/04_debug/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def debug(func):
+    def wrapper(name, age=None):
+        print(func.__name__, name, age)
+        return func(name, age)
+    return wrapper
+
+
+@debug
+def greeting(name, age=None):
+    if age:
+        return "Ого, {name}! Тебе уже {age} лет, ты быстро растёшь!".format(name=name, age=age)
+    else:
+        return "Привет, {name}!".format(name=name)
+
+
+print(greeting("Том"))
+print(greeting("Миша", age=100))
+print(greeting(name="Катя", age=16))
