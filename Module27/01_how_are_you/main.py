6a1805c997bf1331e6a9b510f253f2fda809215a
Vadim <vanderlion@rambler.ru>
2023-02-05 22:26:04 +0400

    added m27 homework, 27.1 fix: how are you

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def how_are_you(func):
+    def wrapper():
+        print("Как твои дела? Хорошо")
+        func()
+        print("А у меня не очень. Ладно держи свою функцию")
+
+    return wrapper
+
+
+@how_are_you
+def test():
+    print('<Тут что-то происходит...>')
+
+
+test()
