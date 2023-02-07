33aa36aa6f8569d952472faa8bcf6e0fe2376f37
Vadim <vanderlion@rambler.ru>
2023-02-07 17:03:52 +0400

    added m28 homework, 28.1 fix: work with file 2

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+class File:
+    """
+    Модернизированная версия контекст-менеджера File — теперь при попытке
+    открыть несуществующий файл менеджер автоматически создаёт и открывает этот файл в режиме записи.
+    """
+
+    def __init__(self, filename: str, mode: str):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)  # Add self.mode
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+
+if __name__ == '__main__':
+    with File('example.txt', 'w+') as file:
+        file.write('Hello World!!')
