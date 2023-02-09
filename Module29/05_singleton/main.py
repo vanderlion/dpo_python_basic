4442b5dbdb5505965d202fe26b9beafdce4cce6d
@vadim_bubnov_1 <vanderlion@rambler.ru>
2023-02-09 23:51:46 +0400

    added M29 homework, 29.5 fix: singleton

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+import functools
+
+
+def singleton(cls):
+    """ декоратор singleton, который превращает класс в одноэлементный"""
+    @functools.wraps(cls)
+    def wrapper_singleton(*args, **kwargs):
+        if not wrapper_singleton.instance:
+            wrapper_singleton.instance = cls(*args, **kwargs)
+
+    wrapper_singleton.instance = None
+    return wrapper_singleton
+
+
+@singleton
+class Example:
+    pass
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
