ef51bf8fbecd57943c91b6f8bd0821fc30ba78f8
@vadim_bubnov_1 <vanderlion@rambler.ru>
2023-02-09 23:41:37 +0400

    added M29 homework, 29.2 fix: callback

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,28 +1,33 @@
-from typing import Callable
 import functools
-
-class CountCalls:
-    """Класс декоратор. Считает количество вызовов функции
-        метод num_calls для базовой функции для счета вызовов
-    """
-    def __init__(self, func: Callable) -> None:
-
-        functools.update_wrapper(self, func)
-        self.func = func
-        self.num_calls = 0
-
-    def __call__(self, *args, **kwargs) -> Callable:
-        self.num_calls += 1
-        print(f'Вызов номер {self.num_calls} функции: {self.func.__name__}')
-        return self.func(*args, **kwargs)
+from typing import Callable, Any, Optional
 
 
-@CountCalls
-def say_hello():
-    print('Hello')
+def callback(route: str) -> Callable:
+    """Декоратор. выполняется после определённого события"""
+    def inner_decorator(func: Callable) -> Callable:
+        app[route] = func # это событие о котором идет речь??
+
+        @functools.wraps(func)
+        def wrapped() -> Any:
+            return func()
+        return wrapped
+    return inner_decorator
 
 
-say_hello()
-say_hello()
-say_hello()
-print(say_hello.num_calls)
+app = {}
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
+
+print(f'Словарь: {app}')
