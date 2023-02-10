2abf79945f783630bb664c7e5aed3ef84209790a
Vadim <vanderlion@rambler.ru>
2023-02-10 22:54:22 +0400

    added m30 homework, 30.4 fix: palindrome the return

--- Module30/04_palindrome_the_return/main.py
+++ Module30/04_palindrome_the_return/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+from collections import Counter
+# возвращает словарь ключ-символ, значение - количество
+
+
+def can_be_poly(string: str) -> bool:
+    """ функция can_be_poly, которая принимает на вход строку
+        и проверяет, можно ли получить из неё палиндром """
+    return len(string) % 2 == sum(x % 2 for x in Counter(string).values())
+    return len(list(filter(lambda x: x % 2, Counter(string).values()))) <= 2
+    return list(filter(lambda x: x % 2, Counter(string).values()))
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
